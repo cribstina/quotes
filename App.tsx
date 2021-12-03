95841d8bfc7a70e9838feb6874fc780e97ee5791
Cristina Souza <cristina@ejcm.com.br>
2021-12-02 22:47:16 -0300

    feature: consuming pexels image api

--- App.tsx
+++ App.tsx
@@ -1,22 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Dimensions, FlatList, Text, View, Image, StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+/* ----- meu código ----- */
+const { width, height } = Dimensions.get('screen');
+import api from './config/api';
+
+const fetchImagesFromPexels = async () => {
+  const data = await fetch(api.API_URL, {
+    headers: {
+      'Authorization': api.API_KEY
+    }
+  })
+
+  const { photos } = await data.json();
+
+  return photos;
+}
+
+
 export default function App() {
+  const [images, setImages] = React.useState(null);
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
+  React.useEffect(() => {
+      const fetchImages = async () => {
+        const images = await fetchImagesFromPexels();
+
+        setImages(images);
+      }
+
+      fetchImages();
+
+  }, [])
+
+ 
+
+  if (!images) {
     return null;
   } else {
+    console.log(images)
     return (
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
+        <FlatList
+          data={images}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => {
+          
+            return <View style={{width, height}}>
+              <Image
+                source={{uri: item.src.landscape}}
+                style={[StyleSheet.absoluteFillObject]} 
+                />
+              </View>
+            }}
+        />
+          
       </SafeAreaProvider>
     );
   }
---
+++ config/api.ts
@@ -0,0 +1,9 @@
+const API_KEY = '563492ad6f917000010000018a02b0a918164fb0a717ae11f916b932';
+
+const API_URL =
+  "https://api.pexels.com/v1/search?query=nature&per_page=80&orientation=landscape";
+
+export default {
+    API_KEY,
+    API_URL
+};
